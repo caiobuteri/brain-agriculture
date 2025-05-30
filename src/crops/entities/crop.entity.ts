6509855e6755b5250ed7e0bbebd85a0fa627cfb4
caio.buteri <caio.buteri@globalsys.com.br>
2025-05-29 22:58:02 -0300

    crops module creation

---
+++ src/crops/entities/crop.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { BaseEntity } from 'src/common/entities/base.entity';
+import { Harvest } from '../../harvests/entities/harvest.entity';
+
+@Entity()
+export class Crop extends BaseEntity {
+  constructor(input?: Partial<Crop>) {
+    super(input);
+    Object.assign(this, input);
+  }
+
+  @Column()
+  name: string;
+
+  @ManyToOne(() => Harvest, (harvest) => harvest.crops, { onDelete: 'CASCADE' })
+  @JoinColumn()
+  harvest: Harvest;
+}
